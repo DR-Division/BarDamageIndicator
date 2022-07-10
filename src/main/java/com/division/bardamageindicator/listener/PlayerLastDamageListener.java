ec687b76ff8de2efd7d558df5598294e4270e069
= <bd2845@naver.com>
2022-07-10 19:41:55 +0900

    투사체도 데미지 처리

--- src/main/java/com/division/bardamageindicator/listener/PlayerLastDamageListener.java
+++ src/main/java/com/division/bardamageindicator/listener/PlayerLastDamageListener.java
@@ -2,9 +2,11 @@ package com.division.bardamageindicator.listener;
 
 import com.division.bardamageindicator.constant.BarConstant;
 import com.division.bardamageindicator.data.BarManager;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -27,11 +29,21 @@ public class PlayerLastDamageListener implements Listener {
         Entity hitter = event.getDamager();
         Entity victim = event.getEntity();
         if (hitter instanceof Player && victim instanceof LivingEntity) {
-            Player p = (Player) hitter;
-            LivingEntity livingVictim = (LivingEntity) victim;
-            double damage = event.getFinalDamage(); //최종데미지를 받는다
-            manager.setBossBar(p, livingVictim, damage);
-            p.setMetadata(BarConstant.BAR_META_KEY, new FixedMetadataValue(Plugin, System.currentTimeMillis())); //최종 엔티티 타격시간을 메타데이터에 저장.
+            handleDamage(hitter, victim, event.getFinalDamage());
         }
+        else if (hitter instanceof Projectile) {
+            Projectile projectile = (Projectile) hitter;
+            if (projectile.getShooter() instanceof Player) {
+                handleDamage(hitter, victim, event.getFinalDamage());
+            }
+        }
+    }
+
+    private void handleDamage(Entity hitter, Entity victim, double damage) {
+        Player p = (Player) hitter;
+        LivingEntity livingVictim = (LivingEntity) victim;
+        manager.setBossBar(p, livingVictim, damage);
+        p.setMetadata(BarConstant.BAR_META_KEY, new FixedMetadataValue(Plugin, System.currentTimeMillis())); //최종 엔티티 타격시간을 메타데이터에 저장.
+
     }
 }
