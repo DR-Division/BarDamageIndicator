8c00ec93a1765b63910bf0b604be3578bda58031
= <bd2845@naver.com>
2022-07-10 19:20:44 +0900

    onEnable 메소드 구성

--- src/main/java/com/division/bardamageindicator/BarDamageIndicator.java
+++ src/main/java/com/division/bardamageindicator/BarDamageIndicator.java
@@ -1,5 +1,10 @@
 package com.division.bardamageindicator;
 
+import com.division.bardamageindicator.constant.BarConstant;
+import com.division.bardamageindicator.data.BarManager;
+import com.division.bardamageindicator.listener.PlayerLastDamageListener;
+import com.division.bardamageindicator.listener.PlayerQuitListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BarDamageIndicator extends JavaPlugin {
@@ -7,7 +12,10 @@ public final class BarDamageIndicator extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        BarManager manager = new BarManager();
+        getServer().getPluginManager().registerEvents(new PlayerLastDamageListener(manager, this), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(manager), this);
+        Bukkit.getScheduler().runTaskTimer(this, manager.new BarRemover(), 0L, BarConstant.BAR_REMOVER_INTERVAL);
     }
 
     @Override
