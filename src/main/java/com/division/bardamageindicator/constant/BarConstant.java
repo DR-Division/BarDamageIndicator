612b570a53c9b42cbe2b690cb4d0e75442ce7735
= <bd2845@naver.com>
2022-07-10 19:20:30 +0900

    runnable용 상수 추가

--- src/main/java/com/division/bardamageindicator/constant/BarConstant.java
+++ src/main/java/com/division/bardamageindicator/constant/BarConstant.java
@@ -3,6 +3,7 @@ package com.division.bardamageindicator.constant;
 public class BarConstant {
 
     public static final String BAR_META_KEY = "bar_indicate";
-    public static final int BAR_REMOVE_DELAY = 10000; //10s
+    public static final long BAR_REMOVE_DELAY = 10000; //10s (long)
+    public static final int BAR_REMOVER_INTERVAL = 40; //2s (tick)
 
 }
