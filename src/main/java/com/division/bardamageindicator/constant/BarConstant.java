8cf120984f323433adba5797a29a8ba96bb01b49
= <bd2845@naver.com>
2022-07-10 19:14:51 +0900

    보스바 전용 정적 클래스

---
+++ src/main/java/com/division/bardamageindicator/constant/BarConstant.java
@@ -0,0 +1,8 @@
+package com.division.bardamageindicator.constant;
+
+public class BarConstant {
+
+    public static final String BAR_META_KEY = "bar_indicate";
+    public static final int BAR_REMOVE_DELAY = 10000; //10s
+
+}
